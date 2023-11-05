c6e9387193ce897d151dc5f2768ea8ff1b4d87c1
Collin Squires <squirrelly392@gmail.com>
2023-11-04 19:06:27 -0600

    added an authentication component

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Auth } from "./components/auth";
+
+function App() {
+  return (
+    <div className="app">
+      <Auth />
+    </div>
+  );
+}
+
+export default App;
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,8 +1,7 @@
-import { initializeApp } from "../node_modules/firebase/app";
-import { getFirestore } from "../node_modules/firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDZDDpTJADUbMWr7TuOVTznpWlvGo-3cgQ",
   authDomain: "uassistant-4f486.firebaseapp.com",
@@ -17,11 +16,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-const db = getFirestore(app);
-
-function addDonor() {
-  addDoc(collection(db, "Donors"), { name: "Steve" });
-}
-
-addDonor();
+export const auth = getAuth(app);
