8126e556193d88178cf15b2882738dbd6f9e8232
Collin Squires <squirrelly392@gmail.com>
2023-11-20 18:11:01 -0700

    new client data gathered

--- scripts/newClient.js
+++ scripts/newClient.js
@@ -7,3 +7,30 @@ cancelButton.addEventListener("click", () => {
         // do nothing
     }
 });
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const formData = new FormData(form);
+    const value = Object.fromEntries(formData.entries());
+    console.log(value);
+
+    const firstName = value.fname;
+    const lastName = value.lname;
+    const gender = value.gender;
+    const dateOfBirth = value.dob;
+
+    const phone = value.phone;
+    const email = value.email;
+    const streetAddress = value.street;
+    const unitNumber = value.unit;
+    const city = value.city;
+    const state = value.state;
+    const zipCode = value.zip;
+
+    const officer = value.officer;
+    const ageGroup = value.ageGroup;
+    const donorType = value.donorType;
+    const testingGroup = value.testingGroup;
+    const activationStaus = value.activationStatus;
+})
