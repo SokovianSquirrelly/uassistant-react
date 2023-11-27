41b2e7eacd2b7da2ef334a53aa7beda11387a311
Collin Squires <squirrelly392@gmail.com>
2023-11-27 00:55:43 -0700

    tweaked the look of the calendar

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -3,10 +3,27 @@ import today from "./dates.js";
 const calendar = document.querySelector("#calendar-body");
 const currentMonth = document.querySelector("#month");
 
+const hideButton = document.querySelector("#hide-calendar");
+const prevButton = document.querySelector("#previous-month");
+const nextButton = document.querySelector("#next-month");
+
 function displayCalendar() {
   currentMonth.textContent = `${today.toLocaleString("default", {
     month: "long",
   })} ${today.getFullYear().toString()}`;
 }
 
-displayCalendar()
+hideButton.addEventListener("click", () => {
+  calendar.classList.toggle("hidden");
+  hideButton.classList.toggle("hidden");
+});
+
+prevButton.addEventListener("click", () => {
+
+});
+
+nextButton.addEventListener("click", () => {
+
+});
+
+displayCalendar();
