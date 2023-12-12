4bdfd9ba043c8604c43b6c7234e867a79b79e837
Collin Squires <squirrelly392@gmail.com>
2023-12-12 08:02:02 -0700

    can now add donors to firebase

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDZDDpTJADUbMWr7TuOVTznpWlvGo-3cgQ",
@@ -97,3 +97,90 @@ function displayDataDesktop(donors) {
   donorTable.appendChild(body);
   donorSection.appendChild(donorTable);
 }
+
+const addDonor = document.getElementById("new-client-form");
+const activationCheckbox = document.getElementById("activationStatus");
+addDonor.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let dob = addDonor.dob.value.split("-");
+  let birthMonth = "";
+  const birthDay = parseInt(dob[2]);
+  const birthYear = parseInt(dob[0]);
+
+  switch (dob[1]) {
+    case "01":
+      birthMonth = "January";
+      break;
+    case "02":
+      birthMonth = "February";
+      break;
+    case "03":
+      birthMonth = "March";
+      break;
+    case "04":
+      birthMonth = "April";
+      break;
+    case "05":
+      birthMonth = "May";
+      break;
+    case "06":
+      birthMonth = "June";
+      break;
+    case "07":
+      birthMonth = "July";
+      break;
+    case "08":
+      birthMonth = "August";
+      break;
+    case "09":
+      birthMonth = "September";
+      break;
+    case "10":
+      birthMonth = "October";
+      break;
+    case "11":
+      birthMonth = "November";
+      break;
+    case "12":
+      birthMonth = "December";
+      break;
+  }
+
+  let isActive = false;
+  if (activationCheckbox.value == "checked") {
+    isActive = true;
+  }
+
+  addDoc(colRef, {
+    activationStatus: isActive,
+    ageGroup: addDonor.ageGroup.value,
+    contactInformation: {
+      address: {
+        addressLine1: addDonor.street.value,
+        addressLine2: addDonor.unit.value,
+        city: addDonor.city.value,
+        state: addDonor.state.value,
+        zip: addDonor.zip.value,
+      },
+      email: addDonor.email.value,
+      phoneNumber: addDonor.phone.value,
+    },
+    dateOfBirth: {
+      day: birthDay,
+      month: birthMonth,
+      year: birthYear,
+    },
+    donorType: addDonor.donorType.value,
+    firstName: addDonor.fname.value,
+    gender: addDonor.gender.value,
+    lastName: addDonor.lname.value,
+    probationOfficer: addDonor.officer.value,
+    testingGroup: addDonor.testingGroup.value,
+  }).then(() => {
+    location.href = "client-manager.html";
+    alert(
+      `${addDonor.fname.value} ${addDonor.lname.value} has been successfully added.`
+    );
+  });
+});
--- scripts/newClient.js
+++ scripts/newClient.js
@@ -8,29 +8,29 @@ cancelButton.addEventListener("click", () => {
     }
 });
 
-const form = document.querySelector("form");
-form.addEventListener("submit", (event) => {
-    event.preventDefault()
-    const formData = new FormData(form);
-    const value = Object.fromEntries(formData.entries());
-    console.log(value);
+// const form = document.querySelector("form");
+// form.addEventListener("submit", (event) => {
+//     event.preventDefault()
+//     const formData = new FormData(form);
+//     const value = Object.fromEntries(formData.entries());
+//     console.log(value);
 
-    const firstName = value.fname;
-    const lastName = value.lname;
-    const gender = value.gender;
-    const dateOfBirth = value.dob;
+//     const firstName = value.fname;
+//     const lastName = value.lname;
+//     const gender = value.gender;
+//     const dateOfBirth = value.dob;
 
-    const phone = value.phone;
-    const email = value.email;
-    const streetAddress = value.street;
-    const unitNumber = value.unit;
-    const city = value.city;
-    const state = value.state;
-    const zipCode = value.zip;
+//     const phone = value.phone;
+//     const email = value.email;
+//     const streetAddress = value.street;
+//     const unitNumber = value.unit;
+//     const city = value.city;
+//     const state = value.state;
+//     const zipCode = value.zip;
 
-    const officer = value.officer;
-    const ageGroup = value.ageGroup;
-    const donorType = value.donorType;
-    const testingGroup = value.testingGroup;
-    const activationStaus = value.activationStatus;
-})
+//     const officer = value.officer;
+//     const ageGroup = value.ageGroup;
+//     const donorType = value.donorType;
+//     const testingGroup = value.testingGroup;
+//     const activationStaus = value.activationStatus;
+// })
