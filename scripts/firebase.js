59c78b73f2e60690fb54e3e2dfaab7d55adb23de
Collin Squires <squirrelly392@gmail.com>
2023-12-12 18:53:14 -0700

    attempted (poorly) to get excused dates underway

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -243,98 +243,116 @@ if (addDonor) {
 const updateDonorForm = document.getElementById("edit-client-form");
 
 const firstNameEdit = document.getElementById("fname-edit");
+const lastNameEdit = document.getElementById("lname-edit");
+const genderEdit = document.getElementById("gender-edit");
+const dobEdit = document.getElementById("dob-edit");
+const phoneEdit = document.getElementById("phone-edit");
+const emailEdit = document.getElementById("email-edit");
+const streetEdit = document.getElementById("street-edit");
+const unitEdit = document.getElementById("unit-edit");
+const cityEdit = document.getElementById("city-edit");
+const stateEdit = document.getElementById("state-edit");
 function editDonor() {
   const urlParams = new URLSearchParams(window.location.search);
   const donorId = urlParams.get("id");
   let data = {};
-  const docRef = doc(db, "DonorsTest", donorId);
+  if (donorId) {
+    const docRef = doc(db, "DonorsTest", donorId);
 
-  getDoc(docRef).then((doc) => {
-    console.log(doc);
-    console.log(doc.data(), doc.id);
-    data = doc.data();
-    firstNameEdit.value = data.firstName;
-  });
-
-  updateDonorForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    let dob = updateDonorForm.dob.value.split("-");
-    let birthMonth = "";
-    const birthDay = parseInt(dob[2]);
-    const birthYear = parseInt(dob[0]);
-
-    switch (dob[1]) {
-      case "01":
-        birthMonth = "January";
-        break;
-      case "02":
-        birthMonth = "February";
-        break;
-      case "03":
-        birthMonth = "March";
-        break;
-      case "04":
-        birthMonth = "April";
-        break;
-      case "05":
-        birthMonth = "May";
-        break;
-      case "06":
-        birthMonth = "June";
-        break;
-      case "07":
-        birthMonth = "July";
-        break;
-      case "08":
-        birthMonth = "August";
-        break;
-      case "09":
-        birthMonth = "September";
-        break;
-      case "10":
-        birthMonth = "October";
-        break;
-      case "11":
-        birthMonth = "November";
-        break;
-      case "12":
-        birthMonth = "December";
-        break;
-    }
-
-    updateDoc(docRef, {
-      activationStatus: activationCheckbox.checked,
-      ageGroup: updateDonorForm.ageGroup.value,
-      contactInformation: {
-        address: {
-          addressLine1: updateDonorForm.street.value,
-          addressLine2: updateDonorForm.unit.value,
-          city: updateDonorForm.city.value,
-          state: updateDonorForm.state.value,
-          zip: updateDonorForm.zip.value,
-        },
-        email: updateDonorForm.email.value,
-        phoneNumber: updateDonorForm.phone.value,
-      },
-      dateOfBirth: {
-        day: birthDay,
-        month: birthMonth,
-        year: birthYear,
-      },
-      donorType: updateDonorForm.donorType.value,
-      firstName: updateDonorForm.fname.value,
-      gender: updateDonorForm.gender.value,
-      lastName: updateDonorForm.lname.value,
-      probationOfficer: updateDonorForm.officer.value,
-      testingGroup: updateDonorForm.testingGroup.value,
-    }).then(() => {
-      location.href = "client-manager.html";
-      alert(
-        `${addDonor.fname.value} ${addDonor.lname.value} has been successfully added.`
-      );
+    getDoc(docRef).then((doc) => {
+      console.log(doc);
+      console.log(doc.data(), doc.id);
+      data = doc.data();
+      firstNameEdit.value = data.firstName;
+      lastNameEdit.value = data.lastName;
+      phoneEdit.value = data.contactInformation.phoneNumber;
+      emailEdit.value = data.contactInformation.email;
+      streetEdit.value = data.contactInformation.address.addressLine1;
+      unitEdit.value = data.contactInformation.address.addressLine2;
+      cityEdit.value = data.contactInformation.address.city;
+      stateEdit.value = data.contactInformation.address.state;
     });
-  });
+
+    updateDonorForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      let dob = updateDonorForm.dob.value.split("-");
+      let birthMonth = "";
+      const birthDay = parseInt(dob[2]);
+      const birthYear = parseInt(dob[0]);
+
+      switch (dob[1]) {
+        case "01":
+          birthMonth = "January";
+          break;
+        case "02":
+          birthMonth = "February";
+          break;
+        case "03":
+          birthMonth = "March";
+          break;
+        case "04":
+          birthMonth = "April";
+          break;
+        case "05":
+          birthMonth = "May";
+          break;
+        case "06":
+          birthMonth = "June";
+          break;
+        case "07":
+          birthMonth = "July";
+          break;
+        case "08":
+          birthMonth = "August";
+          break;
+        case "09":
+          birthMonth = "September";
+          break;
+        case "10":
+          birthMonth = "October";
+          break;
+        case "11":
+          birthMonth = "November";
+          break;
+        case "12":
+          birthMonth = "December";
+          break;
+      }
+
+      updateDoc(docRef, {
+        activationStatus: activationCheckbox.checked,
+        ageGroup: updateDonorForm.ageGroup.value,
+        contactInformation: {
+          address: {
+            addressLine1: updateDonorForm.street.value,
+            addressLine2: updateDonorForm.unit.value,
+            city: updateDonorForm.city.value,
+            state: updateDonorForm.state.value,
+            zip: updateDonorForm.zip.value,
+          },
+          email: updateDonorForm.email.value,
+          phoneNumber: updateDonorForm.phone.value,
+        },
+        dateOfBirth: {
+          day: birthDay,
+          month: birthMonth,
+          year: birthYear,
+        },
+        donorType: updateDonorForm.donorType.value,
+        firstName: updateDonorForm.fname.value,
+        gender: updateDonorForm.gender.value,
+        lastName: updateDonorForm.lname.value,
+        probationOfficer: updateDonorForm.officer.value,
+        testingGroup: updateDonorForm.testingGroup.value,
+      }).then(() => {
+        location.href = "client-manager.html";
+        alert(
+          `${addDonor.fname.value} ${addDonor.lname.value} has been successfully added.`
+        );
+      });
+    });
+  }
 }
 
 function deleteDonor(donorId) {
@@ -350,6 +368,42 @@ function deleteDonor(donorId) {
       location.reload();
     });
   }
+}
+
+const daysOffManager = document.getElementById("schedule-days-off");
+if (daysOffManager) {
+  let donors = [];
+  getDocs(colRef).then((snapshot) => {
+    snapshot.docs.forEach((doc) => {
+      donors.push({ ...doc.data(), id: doc.id });
+    });
+  });
+
+  console.log(donors);
+  donors.forEach((donor) => {
+    const donorName = document.createElement("h3");
+    const daysOffForm = document.createElement("form");
+    const calendar = document.createElement("input");
+    const reason = document.createElement("input");
+    const submit = document.createElement("input");
+
+    donorName.textContent = `${donor.lastName}, ${donor.firstName}`;
+
+    daysOffForm.setAttribute("method", "post");
+
+    calendar.setAttribute("type", "date");
+    calendar.setAttribute("name", "select-day");
+
+    reason.setAttribute("type", "text");
+    reason.setAttribute("name", "reasoning");
+
+    daysOffForm.appendChild(calendar);
+    daysOffForm.appendChild(reason);
+    daysOffForm.appendChild(submit);
+
+    daysOffManager.appendChild(donorName);
+    daysOffManager.appendChild(daysOffForm);
+  });
 }
 
 editDonor();
